41b80b0ba96dabf26a27687e6dc404f6b35455d0
Brian <brian.pardo.gaona@alumnos.upm.es>
2024-04-10 16:31:27 +0200

    Panel de reservas update

--- frontEnd/src/pages/studentPages/Inicio/componentes/PanelReservas.jsx
+++ frontEnd/src/pages/studentPages/Inicio/componentes/PanelReservas.jsx
@@ -10,35 +10,32 @@ function PanelReservas() {
 
     return (
         <div>
-            <p id="title-panel">Panel de Reservas</p>
+            <p id="panel-title">PANEL DE RESERVAS</p>
             <div className="grid">
 
-                <Card>
-                    <CardBody>
-                        <p>Realizar reserva</p>
-                        <img src="/public/realizarReserva.png" alt="realizar-reserva-button" className="w-14 h-14 center"/>
+                <Card className="panel-box">
+                    <CardBody >
+                        <p className="center bold-text big-text">Realizar reserva</p>
+                        <img src="/public/realizarReserva.png" alt="realizar-reserva-button" className="w-14 h-14 center panel-images"/>
 
                     </CardBody>
                     
                 </Card>
 
-                <Card>
-                    <CardBody>
-                        <p>Mis reservas</p>
+                <Card className="panel-box">
+                    <CardBody >
+                        <p className="center bold-text big-text">Modificar reserva</p>
+                        <img src="/public/modificarReserva.png" alt="realizar-reserva-button" className="w-14 h-14 center panel-images"/>
+
                     </CardBody>
                     
                 </Card>
 
-                <Card>
+                <Card className="panel-box">
                     <CardBody>
-                        <p>Opción 3</p>
-                    </CardBody>
-                    
-                </Card>
+                        <p className="center bold-text big-text">Cancelar reserva</p>
+                        <img src="/public/cancelarReserva.png" alt="realizar-reserva-button" className="w-14 h-14 center panel-images"/>
 
-                <Card>
-                    <CardBody>
-                        <p>Opción 4</p>
                     </CardBody>
                     
                 </Card>
