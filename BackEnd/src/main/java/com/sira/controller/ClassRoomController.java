10da319fb119ae475921151da1773325f9f74c6a
davidmartinezbarranco <david.martinez.barranco@alumnos.upm.es>
2024-05-01 11:18:09 +0200

    Nuevo endpoint para ver la disponibilidad de un aula en un mes

--- BackEnd/src/main/java/com/sira/controller/ClassRoomController.java
+++ BackEnd/src/main/java/com/sira/controller/ClassRoomController.java
@@ -1,9 +1,40 @@
 package com.sira.controller;
 
-import jakarta.persistence.Entity;
+import com.sira.model.Classroom;
+import com.sira.model.Reservation;
+import com.sira.repository.ClassroomRepository;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-public class ClassRoomController {
+import java.time.YearMonth;
+import java.util.ArrayList;
+import java.util.List;
 
+@RestController
+@RequestMapping("/classrooms")
+public class ClassroomController {
+    private final ClassroomRepository classroomRepository;
+
+    public ClassroomController(ClassroomRepository classroomRepository) {
+        this.classroomRepository = classroomRepository;
+    }
+
+    @GetMapping("")
+    List<Classroom> allReservations(){return this.classroomRepository.findAll();}
+
+    @GetMapping("{id}/availability/{month}")
+    int[] classroomAvailabilityByMonth(@PathVariable Long id, @PathVariable int month){
+        int[] availabilityArray = new int[YearMonth.of(YearMonth.now().getYear(), month).lengthOfMonth()];
+        for (int i = 0; i < availabilityArray.length; i++) {
+            availabilityArray[i] = 12;
+        }
+
+        List<Reservation> classroomReservations = classroomRepository.findReservationsByMonthAndClassroomId(id, month);
+        for (Reservation reservation : classroomReservations){
+            availabilityArray[reservation.getReservedDay()] -= reservation.getReservedHours();
+        }
+        return availabilityArray;
+    }
 }
---
+++ BackEnd/src/main/java/com/sira/repository/ClassroomRepository.java
@@ -0,0 +1,15 @@
+package com.sira.repository;
+
+import com.sira.model.Classroom;
+import com.sira.model.Reservation;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
+public interface ClassroomRepository extends JpaRepository<Classroom, Long> {
+
+    @Query("SELECT r FROM Reservation r WHERE MONTH(r.startDate) = :month AND r.classroom.id = :classroomId")
+    List<Reservation> findReservationsByMonthAndClassroomId(@Param("classroomId") Long classroomId, @Param("month") int month);
+}
