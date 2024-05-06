a0bff3ba019784e91114e3509696b781ebd4e29a
Brian <brian.pardo.gaona@alumnos.upm.es>
2024-05-06 16:13:27 +0200

    Primera versión final del Login

---
+++ frontEnd/src/pages/LoginPage/components/CustomModal.jsx
@@ -0,0 +1,50 @@
+import React, { useEffect, useState } from "react";
+import { Modal, ModalContent, ModalHeader, ModalBody, ModalFooter, Button, useDisclosure } from "@nextui-org/react";
+
+export default function CustomModal({ text, cargar, onChange, recargarPagina }) {
+    const { isOpen, onOpen, onClose } = useDisclosure();
+    useEffect(() => {
+        if (cargar) {
+            onOpen();
+        } else {
+            onClose();
+        }
+    }, [cargar]);
+
+    const alCerrar = () => {
+        if(recargarPagina === true){
+            window.location.reload();
+        }
+        onClose();
+        onChange(false);
+    }
+
+    return (
+        <>
+
+            <Modal backdrop="blur" isOpen={isOpen} onClose={alCerrar} >
+                <ModalContent>
+                    <>
+                        <ModalHeader className="flex flex-col gap-1">INFORMACIÓN DEL REGISTRO</ModalHeader>
+                        <ModalBody>
+                                {
+                                    text.map((linea, index) => (
+                                        <p key={index}>
+                                            {linea}
+                                        </p>
+                                    ))
+                                }
+                        </ModalBody>
+                        <ModalFooter>
+                            <Button color="primary" onPress={alCerrar}>
+                                De acuerdo
+                            </Button>
+                        </ModalFooter>
+                    </>
+
+                </ModalContent>
+            </Modal>
+        </>
+    );
+}
+
