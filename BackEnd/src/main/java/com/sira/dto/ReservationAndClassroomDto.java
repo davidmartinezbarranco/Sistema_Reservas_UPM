6dd704ae98e091f064aafe7011929d893d3f2060
davidmartinezbarranco <david.martinez.barranco@alumnos.upm.es>
2024-05-11 19:02:22 +0200

    Endpoint get de reservas de usuario modificado para que se vea el id.

--- BackEnd/src/main/java/com/sira/dto/ReservationAndClassroomDto.java
+++ BackEnd/src/main/java/com/sira/dto/ReservationAndClassroomDto.java
@@ -8,12 +8,14 @@ import java.time.LocalDateTime;
 
 @Data
 public class ReservationAndClassroomDto {
+    private Long id;
     private LocalDateTime startDate;
     private LocalDateTime endDate;
     private Classroom classroom;
     private Long userId;
 
     public ReservationAndClassroomDto(Reservation reservation) {
+        this.id = reservation.getId();
         this.startDate = reservation.getStartDate();
         this.endDate = reservation.getEndDate();
         this.classroom = reservation.getClassroom();
