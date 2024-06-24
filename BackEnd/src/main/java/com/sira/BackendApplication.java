d824c89dcacce225d42df14e4fcdcaf4742283e7
davidmartinezbarranco <david.martinez.barranco@alumnos.upm.es>
2024-06-24 18:39:00 +0200

    Algunos retoques

--- BackEnd/src/main/java/com/sira/BackendApplication.java
+++ BackEnd/src/main/java/com/sira/BackendApplication.java
@@ -1,13 +1,7 @@
 package com.sira;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication()
 public class BackendApplication {
